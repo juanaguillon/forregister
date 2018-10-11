305d4b54400d3968c9c8a3932cb37aa7e8df861d
Juan Aguillon <miloca.98@hotmail.com>
2018-10-11 00:42:08 -0500

    Añadiendo Pug y rutas JS

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const app = express();
 const urlencoded = parser.urlencoded({extended: false});
 const json = parser.json();
 
-app.use( urlencoded )
+app.use( urlencoded );
 app.use( json );
 
+app.set('views','./views');
+app.set('view engine','pug');
+
 app.set('port', process.env.PORT || 3000 );
+app.use('/public', express.static( __dirname + '/views/scripts' ) ) ;
+app.get('/pugger', ( req, res ) => {
+  // Enviar un titulo a la plantilla index de views.
+  res.render('index',{title:'Ho mamn'})
+})
 app.listen( app.get('port'), function(){
   console.log(`Conectado en puerto localhost: ${app.get('port')}`);
-})
+});
+
