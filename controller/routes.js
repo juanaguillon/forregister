859a1d8c65fb2bae12b2e879a5a8711d02b058aa
Juan Aguillon <miloca.98@hotmail.com>
2018-10-28 16:15:32 -0500

    Agregando archivo de Rutas

---
+++ controller/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const urlEncoded = bodyParser.urlencoded( {extended:false} );
+const json = bodyParser.json();
+
+// Process the request to json.
+// Is important for applicacion can be read the body data.
+app.use( json );
+// Request body is URL Encoded
+app.use( urlEncoded );
+
+// Set engine view.
+app.set('view engine','pug');
+// Set the port to listen the app
+app.set('port', process.env.PORT || 3000 );
+
+/**
+ * The routes to listen the browser
+ * HTTP request GET listening in app.get function
+ */
+app.get('/pugger', (req, res) => {
+  console.log(req.body)
+  res.render('index', { title: 'Ho mamn' });
+})
+
+module.exports = {
+  express: express,
+  app: app
+
+}
--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const express = require('express');
-const parser = require('body-parser');
-const app = express();
-const urlencoded = parser.urlencoded({extended: false});
-const json = parser.json();
+const routes = require('./controller/routes'); 
 
-app.use( urlencoded );
-app.use( json );
+// Set the source views as publics paths
+// Remembering that, the routes is the module routes exporting.
+routes.app.use('/publics', routes.express.static( '/views' ) );
 
-app.set('views','./views');
-app.set('view engine','pug');
-
-app.set('port', process.env.PORT || 3000 );
-app.use('/public', express.static( __dirname + '/views/scripts' ) ) ;
-app.get('/pugger', ( req, res ) => {
-  // Enviar un titulo a la plantilla index de views.
-  res.render('index',{title:'Ho mamn'})
-})
-app.listen( app.get('port'), function(){
-  console.log(`Conectado en puerto localhost: ${app.get('port')}`);
+/**
+ * The port to listen the application.
+ */
+routes.app.listen(routes.app.get('port'), function(){
+  console.log(`Conectado en puerto localhost: ${routes.app.get('port')}`);
 });
 
