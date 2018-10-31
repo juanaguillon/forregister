b492d9e2b65f00ae0e13594eabf22039d18a329c
Juan Aguillon <miloca.98@hotmail.com>
2018-10-30 23:52:58 -0500

    Gulp Semantic

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const routes = require('./controller/routes');
 
 // Set the source views as publics paths
 // Remembering that, the routes is the module routes exporting.
-routes.app.use('/publics', routes.express.static( '/views' ) );
+routes.app.use('/publics', routes.express.static( './views' ) );
+routes.app.use('/publics/semantic/', routes.express.static( './semantic' ) );
 
 /**
  * The port to listen the application.
