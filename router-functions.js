de7237094431beb28b7740e429827bfc6728dc95
Juan Aguillon <miloca.98@hotmail.com>
2018-11-04 20:48:13 -0500

    Revisando PUG

--- router-functions.js
+++ router-functions.js
@@ -5,15 +5,15 @@ class RouterFunctions {
   registerUser( req, res ){
     res.render( 'register', {title: "Registro de Usuario" } )
     let userModel = model.getModel('user')
-    let user = new userModel({
-      name: req.query.name,
-      email: 'joel@gmail.com',
-      password: '1234'
-    });
+    // let user = new userModel({
+    //   name: req.query.name,
+    //   email: 'joel@gmail.com',
+    //   password: '1234'
+    // });
 
-    user.save( err => {
-      if ( err ) throw err;
-    })
+    // user.save( err => {
+    //   if ( err ) throw err;
+    // })
   }   
   
 }
