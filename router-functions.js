37124deb52499bb761b57e6682f1df04170b8c8b
Juan Aguillon <miloca.98@hotmail.com>
2018-11-05 12:17:36 -0500

    Registro Suspendido

--- router-functions.js
+++ router-functions.js
@@ -4,16 +4,6 @@ class RouterFunctions {
   
   registerUser( req, res ){
     res.render( 'register', {title: "Registro de Usuario" } )
-    let userModel = model.getModel('user')
-    // let user = new userModel({
-    //   name: req.query.name,
-    //   email: 'joel@gmail.com',
-    //   password: '1234'
-    // });
-
-    // user.save( err => {
-    //   if ( err ) throw err;
-    // })
   }   
   
 }
