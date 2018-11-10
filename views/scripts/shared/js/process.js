d08362c123f43e5a52b3828cddee7d7f37d4d798
Juan Aguillon <miloca.98@hotmail.com>
2018-11-09 22:41:29 -0500

    Agregando Clase Process

--- views/scripts/shared/js/process.js
+++ views/scripts/shared/js/process.js
@@ -1,19 +1,38 @@
 "use strict"
 var process = function( ){
 
+  /**
+   * Make a bucle for a object ( Key : Value)
+   * The callback parameter passed two parametes. First parameter is "key" , and second "value" parameter for get the values returned in loop.
+   */
+
   Object.prototype.newBucle = function( object, callback ){
-    for ( key in object ){
+    for ( var key in object ){
       if ( object.hasOwnProperty( key ) ){
-        callback();
+        callback( key, object );
       }
     }
   }  
 
-  this.secureTypes = function (types) {
+  /**
+   * Makes secure a types of parameters.
+   * If not is secure, lose the value of value in types ( parameter ) json, and replace by 'Secure Failed' string
+   * For send a types parameter, you need send json structure like that:
+   * var a = {
+   *  [String] // The type requiered in the value... Necessary the brackets.
+   *  value // May is passed like variable
+   * }
+   * 
+   */
+  this.secureTypes = function ( types ) {
     if (types.constructor == Object) {
-
-      Object.newBucle( types, function( ){
-        
+      var secureType = {};
+      Object.newBucle( types, function( key, obj ){
+        if ( obj[key].constructor == key  ){
+          secureType.push( obj[key] )
+        }else{
+          secureType.push(obj[key] = 'Secure Failed' )
+        }
       })
 
     } else {
