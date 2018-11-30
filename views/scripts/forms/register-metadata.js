14ebc9d9e1601a28d012663d736209959d479ba3
Juan Aguillon <miloca.98@hotmail.com>
2018-11-29 23:44:31 -0500

    Actualizando register-metadata | uso función sendLocation

--- views/scripts/forms/register-metadata.js
+++ views/scripts/forms/register-metadata.js
@@ -109,8 +109,7 @@ var register_metadata = function( form ){
     .then( response => response.json() )
     .then( res => {
       if ( res.stat ){
-        console.log( res )
-        window.location.href = "/register-success";
+        sendLocation("register-success");
       }else if( ! res.stat && res.message ){
         $(this.form).before(
           this.formProcess.createError(res.message )
